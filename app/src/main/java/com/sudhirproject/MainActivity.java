84b45af8d381d447b488f671a4b2e3c30a50b8a2
sidrwt <iamsudhirrawat@gmail.com>
2016-05-06 12:54:09 +0530

    successfully done.

--- app/src/main/java/com/sudhirproject/MainActivity.java
+++ app/src/main/java/com/sudhirproject/MainActivity.java
@@ -4,13 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title = (TextView) findViewById(R.id.title);
     }
 
     @Override
