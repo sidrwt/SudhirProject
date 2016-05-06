bdc19f19aa455bb5b164c7711ef944f951417a17
sidrwt <iamsudhirrawat@gmail.com>
2016-05-06 13:03:58 +0530

    successfully done.

--- app/src/main/java/com/sudhirproject/SecondScreen.java
+++ app/src/main/java/com/sudhirproject/SecondScreen.java
@@ -1,38 +1,22 @@
 package com.sudhirproject;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
-public class MainActivity extends Activity {
-
-    private TextView title;
+public class SecondScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        title = (TextView) findViewById(R.id.title);
-
-        title.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent i = new Intent(MainActivity.this, SecondScreen.class);
-                startActivity(i);
-            }
-        });
+        setContentView(R.layout.activity_second_screen);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_second_screen, menu);
         return true;
     }
 
